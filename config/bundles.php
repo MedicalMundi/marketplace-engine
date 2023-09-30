a5b20730a54623d4a6b1e1130cab7e8ef96afcf7
zero <teclaizerai@gmail.com>
2023-09-30 12:45:13 +0200

    database conf (#3)
    * Composer: added symfony/orm-pack.
    
    * Update doctrine conf.
    
    * CI: init acceptance-stage workflow.

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Corley\MaintenanceBundle\CorleyMaintenanceBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
