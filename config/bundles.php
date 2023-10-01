becbeda3234b87ca566858da793c24aa969ed1e2
zero <teclaizerai@gmail.com>
2023-10-01 19:35:11 +0200

    ecotone (#6)
    * Composer: added ecotone/symfony-bundle.
    
    * Composer: added ecotone/jms-converter.
    
    * Composer: added ecotone/dbal.
    
    * Composer: added ecotone/pdo-event-sourcing with default recipes.
    
    * CI: drop support for php-8.3.

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Corley\MaintenanceBundle\CorleyMaintenanceBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Ecotone\SymfonyBundle\EcotoneSymfonyBundle::class => ['all' => true],
 ];
