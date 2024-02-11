9298fc95790f5e37adbf7738af533727c6531f63
zerai <teclaizerai@gmail.com>
2024-02-11 16:04:34 +0100

    bffWeb: conf home controller.

---
+++ _bffWeb/src/AdapterForWeb/HomeController.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the medicalmundi/marketplace-engine
+ *
+ * @copyright (c) 2023 MedicalMundi
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * {@link https://github.com/medicalmundi/marketplace-engine/graphs/contributors developer} and is licensed under the MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * @license https://github.com/MedicalMundi/marketplace-engine/blob/main/LICENSE MIT
+ */
+
+namespace BffWeb\AdapterForWeb;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[AsController]
+#[isGranted('ROLE_ADMIN')]
+class HomeController extends AbstractController
+{
+    #[Route('/web', name: 'web_home')]
+    public function index(): Response
+    {
+        return $this->render('@web/home/index.html.twig');
+    }
+}
