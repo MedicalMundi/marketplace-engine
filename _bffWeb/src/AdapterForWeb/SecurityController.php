3cc9466723bf706a0a9bfbbddff07986ba796bac
zero <teclaizerai@gmail.com>
2024-02-14 11:18:01 +0100

    refactoring security login (#29)
    * Update security.yaml.
    
    * Template: update login.html.twig.
    
    * Templates: Update layout and common templates.
    
    * Refatoring bffWeb templates.
    
    * Move security controller in _bffWeb.
    
    * Tool: update phparkitect whitelist.
    
    * Refactoring logut based on 6.4 fetures

--- _bffWeb/src/AdapterForWeb/SecurityController.php
+++ _bffWeb/src/AdapterForWeb/SecurityController.php
@@ -13,7 +13,7 @@
  * @license https://github.com/MedicalMundi/marketplace-engine/blob/main/LICENSE MIT
  */
 
-namespace App\Controller;
+namespace BffWeb\AdapterForWeb;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,15 +31,9 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', [
+        return $this->render('@web/security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
-    }
-
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
