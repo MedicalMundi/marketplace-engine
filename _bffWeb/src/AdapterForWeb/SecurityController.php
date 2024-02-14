e4cdaf7136fd95e995617ffc9a26a03d168b4f85
zerai <teclaizerai@gmail.com>
2024-02-14 10:46:33 +0100

    Move security controller in _bffWeb.

--- _bffWeb/src/AdapterForWeb/SecurityController.php
+++ _bffWeb/src/AdapterForWeb/SecurityController.php
@@ -13,7 +13,7 @@
  * @license https://github.com/MedicalMundi/marketplace-engine/blob/main/LICENSE MIT
  */
 
-namespace App\Controller;
+namespace BffWeb\AdapterForWeb;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,7 +31,7 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', [
+        return $this->render('@web/security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
