d56f03930e84e00cda7a82a27703107da34e7ad1
zerai <teclaizerai@gmail.com>
2024-02-14 19:08:51 +0100

    Init ecotone conf.

---
+++ _catalog/src/Infrastructure/EcotoneConfiguration.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the medicalmundi/marketplace-engine
+ *
+ * @copyright (c) 2023 MedicalMundi
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * {@link https://github.com/medicalmundi/marketplace-engine/graphs/contributors developer} and is licensed under the MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * @license https://github.com/MedicalMundi/marketplace-engine/blob/main/LICENSE MIT
+ */
+
+namespace Catalog\Infrastructure;
+
+use Ecotone\Dbal\Configuration\DbalConfiguration;
+use Ecotone\Dbal\DbalBackedMessageChannelBuilder;
+use Ecotone\Messaging\Attribute\ServiceContext;
+
+class EcotoneConfiguration
+{
+    #[ServiceContext]
+    public function getDbalConfiguration(): DbalConfiguration
+    {
+        return DbalConfiguration::createWithDefaults()
+            ->withDoctrineORMRepositories(true);
+    }
+
+    #[ServiceContext]
+    public function catalogChannel(): DbalBackedMessageChannelBuilder
+    {
+        return DbalBackedMessageChannelBuilder::create("catalog");
+    }
+}
