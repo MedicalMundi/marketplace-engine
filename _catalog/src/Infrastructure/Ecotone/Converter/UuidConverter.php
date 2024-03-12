a1be0ac4dfb83e59ff65117d4b5a9c7edc6718c5
zerai <teclaizerai@gmail.com>
2024-03-12 16:26:44 +0100

    Init Catalog\Catalog.

---
+++ _catalog/src/Infrastructure/Ecotone/Converter/UuidConverter.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the medicalmundi/marketplace-engine
+ *
+ * @copyright (c) 2023 MedicalMundi
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * {@link https://github.com/medicalmundi/marketplace-engine/graphs/contributors developer} and is licensed under the MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * @license https://github.com/MedicalMundi/marketplace-engine/blob/main/LICENSE MIT
+ */
+
+namespace Catalog\Infrastructure\Ecotone\Converter;
+
+use Ecotone\Messaging\Attribute\Converter;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
+class UuidConverter
+{
+    #[Converter]
+    public function from(UuidInterface $status): string
+    {
+        return $status->toString();
+    }
+
+    #[Converter]
+    public function to(string $status): UuidInterface
+    {
+        return Uuid::fromString($status);
+    }
+}
