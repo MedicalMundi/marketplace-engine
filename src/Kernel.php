0b2c7633ee09e65714184a6313dc7058f79d616f
zerai <teclaizerai@gmail.com>
2024-07-31 17:28:44 +0200

    Register 'bffApi' module as symfony extension.

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,17 @@
 
 namespace App;
 
+use BffApi\Infrastructure\Framework\Extension\BffApiModuleExtension;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function build(ContainerBuilder $container): void
+    {
+        $container->registerExtension(new BffApiModuleExtension());
+    }
 }
