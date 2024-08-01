9537a4eb5f94a725e2e8282c63347f860136d190
zerai <teclaizerai@gmail.com>
2024-08-01 14:38:07 +0200

    Tool: update phparkitect conf.

--- tools/phparkitect/VendorDependencies/allowed_in_bffApi_adapters.php
+++ tools/phparkitect/VendorDependencies/allowed_in_bffApi_adapters.php
@@ -1,5 +1,11 @@
 <?php declare(strict_types=1);
 
 return [
+    'Symfony\Component\HttpFoundation\JsonResponse',
+    'Symfony\Component\HttpFoundation\Request',
+    'Symfony\Bundle\FrameworkBundle\Controller\AbstractController',
 
+    'BffApi\Infrastructure\OperationOrientedApi\ValidationHandler',
+    'BffApi\Infrastructure\OperationOrientedApi\ApiOperationHandler',
+    'BffApi\Infrastructure\OperationOrientedApi\ApiInput',
 ];
