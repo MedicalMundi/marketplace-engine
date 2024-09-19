f5eebe9b7ebc18be19d5271d0c176bd69065df05
zerai <teclaizerai@gmail.com>
2024-09-19 14:45:28 +0200

    init update metadata as process.

---
+++ _metadata/src/AdapterCli/ForSynchronizingMetadata/MetadataUpdateCommand.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the medicalmundi/marketplace-engine
+ *
+ * @copyright (c) 2024 MedicalMundi
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * {@link https://github.com/medicalmundi/marketplace-engine/graphs/contributors developer} and is licensed under the MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * @license https://github.com/MedicalMundi/marketplace-engine/blob/main/LICENSE MIT
+ */
+
+namespace Metadata\AdapterCli\ForSynchronizingMetadata;
+
+use Ecotone\Messaging\Attribute\ConsoleCommand;
+use Ecotone\Modelling\CommandBus;
+use Metadata\AdapterForReadingExternalMetadataSource\UpdateModuleMetadata;
+
+class MetadataUpdateCommand
+{
+    #[ConsoleCommand('metadata:module:update')]
+    public function execute(CommandBus $commandBus): void
+    {
+        $moduleId = 'foo';
+
+        // with error
+        $repoUrl = 'https://github.com/zerai/foo';
+
+        // without metadata
+        //$repoUrl = 'https://github.com/zerai/oe-module-demo-farm-add-ons';
+
+        // with metadata
+        //$repoUrl = 'https://github.com/MedicalMundi/oe-module-todo-list';
+
+        $command = new UpdateModuleMetadata($moduleId, $repoUrl);
+
+        $commandBus->send($command);
+    }
+}
--- _metadata/src/AdapterForReadingExternalMetadataSource/GithubAdapterForReadingExternalMetadataSource.php
+++ _metadata/src/AdapterForReadingExternalMetadataSource/GithubAdapterForReadingExternalMetadataSource.php
@@ -1,4 +1,17 @@
-<?php
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the medicalmundi/marketplace-engine
+ *
+ * @copyright (c) 2024 MedicalMundi
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * {@link https://github.com/medicalmundi/marketplace-engine/graphs/contributors developer} and is licensed under the MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * @license https://github.com/MedicalMundi/marketplace-engine/blob/main/LICENSE MIT
+ */
 
 namespace Metadata\AdapterForReadingExternalMetadataSource;
 
@@ -9,21 +22,19 @@ use Metadata\Core\Port\Driven\ForReadingExternalMetadataSource\MetadataReaderExc
 
 class GithubAdapterForReadingExternalMetadataSource implements ForReadingExternalMetadataSource
 {
-
-    public function __construct(private readonly GithubClient $githubClient)
-    {
+    public function __construct(
+        private readonly GithubClient $githubClient
+    ) {
     }
 
     public function readMetadataFromExternalSource(string $moduleUrl): ExternalMetadataDto
     {
-
         try {
-            // TODO: Implement readMetadataFromExternalSource() method.
             //get composer.json
             // extract metadata section
             // create metadataDto
-            return new ExternalMetadataDto(true,'a category', ['tag-1','tag-2']);
-        }catch (\RuntimeException $exception){
+            return new ExternalMetadataDto(true, 'a category', ['tag-1', 'tag-2']);
+        } catch (\RuntimeException $exception) {
             throw new MetadataReaderException($exception->getMessage());
         }
     }
